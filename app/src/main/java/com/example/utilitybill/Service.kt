7bb80d3e40f374a4482c83d9a4ea8b6c3107ec24
unknown <serhiiromanchuk@gmail.com>
2023-02-18 10:29:21 +0200

    create ServiceDatabase

--- app/src/main/java/com/example/utilitybill/Service.kt
+++ app/src/main/java/com/example/utilitybill/Service.kt
@@ -1,8 +1,21 @@
 package com.example.utilitybill
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "service")
 data class Service(
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
     val id: Int,
+
+    @ColumnInfo(name = "name")
     val name: String,
+
+    @ColumnInfo(name = "tariff")
     val tariff: Double,
+
+    @ColumnInfo(name = "previous_value")
     val previousValue: Int
 )
---
+++ app/src/main/java/com/example/utilitybill/ServiceDatabase.kt
@@ -0,0 +1,40 @@
+package com.example.utilitybill
+
+import android.app.Application
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Database(entities = [Service::class], version = 1, exportSchema = false)
+abstract class ServiceDatabase : RoomDatabase() {
+    abstract fun serviceDao(): ServiceDao
+
+    companion object {
+        private const val DB_NAME = "service.db"
+        @Volatile
+        private var INSTANCE: ServiceDatabase? = null
+
+        fun getDatabase(application: Application): ServiceDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    application,
+                    ServiceDatabase::class.java,
+                    DB_NAME
+                ).fallbackToDestructiveMigration().build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
+
+@Dao
+interface ServiceDao {
+    @Query("SELECT * FROM service")
+    fun getServices(): LiveData<List<Service>>
+
+    @Insert
+    fun addService(service: Service)
+
+    @Query("DELETE FROM service WHERE id = :id")
+    fun removeService(id: Int)
+}
