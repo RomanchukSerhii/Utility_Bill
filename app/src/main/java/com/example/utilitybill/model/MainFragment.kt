57264735a1aaf470f3eaa788f752ecf46cf03f39
unknown <serhiiromanchuk@gmail.com>
2023-02-26 20:31:43 +0200

    edit card number implementation

--- app/src/main/java/com/example/utilitybill/model/MainFragment.kt
+++ app/src/main/java/com/example/utilitybill/model/MainFragment.kt
@@ -1,14 +1,19 @@
 package com.example.utilitybill.model
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
-import android.util.Log
+import android.text.Editable
+import android.text.TextWatcher
+import android.text.method.DigitsKeyListener
+import android.view.KeyEvent
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
 import android.widget.EditText
 import android.widget.TextView
-import android.widget.Toast
 import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
@@ -17,14 +22,17 @@ import com.example.utilitybill.R
 import com.example.utilitybill.database.Service
 import com.example.utilitybill.databinding.FragmentMainBinding
 import com.google.android.material.card.MaterialCardView
-import com.google.android.material.textfield.TextInputLayout
 
+const val APP_PREFERENCES = "APP_PREFERENCES"
+const val PREF_CARD_NUMBER_VALUE = "PREF_CARD_NUMBER_VALUE"
 
 class MainFragment : Fragment() {
     private var _binding: FragmentMainBinding? = null
     private val binding: FragmentMainBinding
         get() = _binding ?: throw RuntimeException("FragmentMainBinding == null")
+
     private lateinit var serviceAdapter: ServiceAdapter
+    private lateinit var preferences: SharedPreferences
     private val viewModel: MainViewModel by activityViewModels()
 
     override fun onCreateView(
@@ -32,6 +40,7 @@ class MainFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentMainBinding.inflate(inflater, container, false)
+        preferences = requireActivity().getSharedPreferences(APP_PREFERENCES, Context.MODE_PRIVATE)
         return binding.root
     }
 
@@ -48,13 +57,111 @@ class MainFragment : Fragment() {
         )
         observeViewModels()
         binding.recyclerView.adapter = serviceAdapter
-        binding.buttonAddService.setOnClickListener { goToSaveService() }
-        binding.buttonCreateBill.setOnClickListener { goToResult() }
+        setListeners()
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun setListeners() {
+        val cardNumber = preferences.getString(
+            PREF_CARD_NUMBER_VALUE, "0000 0000 0000 0000"
+        )
+        cardNumber?.let { binding.textViewCardNumber.text = changeCardNumberToSecure(cardNumber) }
+
+        binding.apply {
+            buttonAddService.setOnClickListener { goToSaveService() }
+
+            buttonCreateBill.setOnClickListener { goToResult() }
+
+            imageViewEditCard.setOnClickListener {
+                val currentCardNumber = preferences.getString(
+                    PREF_CARD_NUMBER_VALUE, "0000 0000 0000 0000"
+                )
+                editTextCardNumber.visibility = View.VISIBLE
+                editTextCardNumber.setText(currentCardNumber)
+                editTextCardNumber.requestFocus()
+
+                imageViewEditCardDone.visibility = View.VISIBLE
+                textViewCardNumber.visibility = View.GONE
+                imageViewEditCard.visibility = View.GONE
+            }
+
+            imageViewEditCardDone.setOnClickListener {
+                val cardEditNumber = editTextCardNumber.text.toString()
+                if (cardEditNumber.length == CARD_NUMBER_LENGTH) {
+                    saveCurrentNumber(cardEditNumber)
+                } else {
+                    editTextCardNumber.error = resources.getString(R.string.card_number_value_error)
+                }
+            }
+
+            editTextCardNumber.setOnFocusChangeListener { _, hasFocus ->
+                if (!hasFocus) {
+                    val currentCardNumber = editTextCardNumber.text.toString()
+                    if (currentCardNumber.length == CARD_NUMBER_LENGTH) {
+                        saveCurrentNumber(currentCardNumber)
+                    } else {
+                        cardNumber?.let {
+                            saveCurrentNumber(cardNumber)
+                        }
+                    }
+                }
+            }
+
+            editTextCardNumber.addTextChangedListener(object : TextWatcher {
+                private var isFormatting: Boolean = false
+                private val spacer = " "
+
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) { }
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) { }
+
+                override fun afterTextChanged(s: Editable?) {
+                    if (isFormatting) {
+                        return
+                    }
+
+                    isFormatting = true
+                    if (s != null && s.length == CARD_NUMBER_LENGTH + 1) {
+                        s.delete(CARD_NUMBER_LENGTH, CARD_NUMBER_LENGTH + 1)
+                    }
+                    val cleanString = s.toString().replace(spacer, "")
+                    val formattedString = StringBuilder()
+
+
+                    for (i in cleanString.indices) {
+                        formattedString.append(cleanString[i])
+                        if ((i + 1) % 4 == 0 && i != cleanString.length - 1) {
+                            formattedString.append(spacer)
+                        }
+                    }
+
+                    binding.editTextCardNumber.setText(formattedString)
+                    binding.editTextCardNumber.setSelection(formattedString.length)
+                    isFormatting = false
+                }
+            })
+        }
+    }
+
+    private fun saveCurrentNumber(cardEditNumber: String) {
+        preferences.edit()
+            .putString(PREF_CARD_NUMBER_VALUE, cardEditNumber)
+            .apply()
+        binding.apply {
+            textViewCardNumber.text = changeCardNumberToSecure(cardEditNumber)
+            editTextCardNumber.visibility = View.GONE
+            imageViewEditCardDone.visibility = View.GONE
+            textViewCardNumber.visibility = View.VISIBLE
+            imageViewEditCard.visibility = View.VISIBLE
+        }
+    }
+
+    private fun changeCardNumberToSecure(cardNumber: String): String {
+        return ("${cardNumber.take(4)} **** **** ${cardNumber.takeLast(4)}")
     }
 
     private fun goToSaveService() {
@@ -143,5 +250,9 @@ class MainFragment : Fragment() {
             editTextCurrentValue.error = resources.getString(R.string.current_value_error)
         }
 
+    }
+
+    companion object {
+        private const val CARD_NUMBER_LENGTH = 19
     }
 }
